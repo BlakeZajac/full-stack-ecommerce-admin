c3909c5de1e352cf271520fc0aa61d3577d01868
Blake Zajac <contact@blakezajac.com>
2023-07-21 20:36:50 +1000

    Settings form updates

--- app/(dashboard)/[storeId]/(routes)/settings/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/settings-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { store } from "@prisma/client";
+import { Trash } from "lucide-react";
 
 interface SettingsFormProps {
   initialData: store;
@@ -10,7 +12,10 @@ interface SettingsFormProps {
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   return (
     <div className="flex items-center justify-between">
-      <Heading title="Settings" description="Manage store preferences." />
+      <Heading title="Settings" description="Manage store preferences" />
+      <Button variant="destructive" size="sm" onClick={() => {}}>
+        <Trash className="h-4 w-4" />
+      </Button>
     </div>
   );
 };
