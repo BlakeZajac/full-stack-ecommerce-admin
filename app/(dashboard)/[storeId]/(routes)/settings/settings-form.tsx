65e54c4025a602a91c5e50dff2948f174b63c85c
Blake Zajac <contact@blakezajac.com>
2023-07-21 20:05:08 +1000

    Set up store switcher, navigation and settings

---
+++ app/(dashboard)/[storeId]/(routes)/settings/settings-form.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { store } from "@prisma/client";
+
+interface SettingsFormProps {
+  initialData: store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Heading title="Settings" description="Manage store preferences." />
+    </div>
+  );
+};
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,31 @@
-import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <div>This will be the store switcher</div>
+      <div className="flex h-16 items-center p-4">
+        <StoreSwitcher items={stores} />
 
-        <div>This will be the routes</div>
+        <MainNav className="mx-6" />
 
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
