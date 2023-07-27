3620f809caf95a94136b4b0f48e9adb9b84998af
Blake Zajac <contact@blakezajac.com>
2023-07-27 17:28:14 +1000

    Remove SFTP folder

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -40,7 +40,7 @@ export function MainNav({
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
-    },  
+    },
   ];
 
   return (
